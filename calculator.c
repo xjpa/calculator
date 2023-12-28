3ec79fb4d9fd1038e7e8c98c7ce43ff6861d6b68
xjpa <xjpa@proton.me>
2023-12-28 11:17:58 +0800

    add more functions to calculator, to support later

--- calculator.c
+++ calculator.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 
+// arithmetic
 int add(int a, int b)
 {
     return a + b;
@@ -23,6 +25,52 @@ int divide(int a, int b)
     }
     else
     {
-        return -1; // error case
+        return -1; // error case for division by zero
     }
+}
+
+// some advanced math functions
+double power(double base, double exponent)
+{
+    return pow(base, exponent);
+}
+
+double square_root(double number)
+{
+    if (number < 0)
+    {
+        return -1; // error case for square root of negative number
+    }
+    return sqrt(number);
+}
+
+// utility, etc.
+int factorial(int number)
+{
+    if (number < 0)
+    {
+        return -1; // error case for negative numbers
+    }
+    int result = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int is_prime(int number)
+{
+    if (number <= 1)
+    {
+        return 0; // not prime
+    }
+    for (int i = 2; i * i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            return 0; // not prime
+        }
+    }
+    return 1; // returnprime
 }
